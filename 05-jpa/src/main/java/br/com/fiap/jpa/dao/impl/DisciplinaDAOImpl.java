db0e21d957b7a6d590476d6db033ae4d05c09913
Paulo Cesar Abrantes <pc.abrantes@gmail.com>
2022-03-28 00:11:12 -0300

    Update DisciplinaDAOImpl.java

--- 05-jpa/src/main/java/br/com/fiap/jpa/dao/impl/DisciplinaDAOImpl.java
+++ 05-jpa/src/main/java/br/com/fiap/jpa/dao/impl/DisciplinaDAOImpl.java
@@ -4,13 +4,13 @@ import br.com.fiap.jpa.entity.Disciplina;
 
 public class DisciplinaDAOImpl extends HibernateGenericDAO<Disciplina, Long> {
 
-	private DisciplinaDAOImpl instance;
+	private static DisciplinaDAOImpl instance;
 	
 	private DisciplinaDAOImpl() {
 		super(Disciplina.class);
 	}
 	
-	public DisciplinaDAOImpl getInstance() {
+	public static DisciplinaDAOImpl getInstance() {
 		
 		if (instance == null) {
 			instance = new DisciplinaDAOImpl();
